7ad2aa952baa4d4f01a616e901970832615a59c5
PurpleDNA <kadirimaroof@gmail.com>
2025-03-02 13:34:24 +0100

    refactor LoginModal and LoginModalOptions for improved styling and responsiveness

--- frontend/src/components/Modals/LoginModal.tsx
+++ frontend/src/components/Modals/LoginModal.tsx
@@ -1,9 +1,9 @@
 // import React from 'react'
 import LoginModalOptions from "./LoginModalOptions";
-import { ModalOptionsProps } from "./LoginModalOptions";
-import nfid from "../../assets/nfid.png";
+// import { ModalOptionsProps } from "./LoginModalOptions";
+// import nfid from "../../assets/nfid.png";
 import ethereum from "../../assets/ethereum.png";
-import identity from "../../assets/identity.png";
+// import identity from "../../assets/identity.png";
 import { X } from "lucide-react";
 import { useContext } from "react";
 import { Context } from "../../Context/context";
@@ -13,19 +13,19 @@ function LoginModal() {
   const { visible, setVisible } = useModal;
   return (
     <div
-      className={`w-screen h-screen bg-[#222226] fixed top-0 left-0 z-50 flex justify-center items-center bg-opacity-80 backdrop-filter backdrop-blur-sm ${
+      className={`w-screen h-screen bg-[#222226] fixed inset-0 top-0 left-0 z-50 flex justify-center items-center bg-opacity-80 backdrop-blur-sm ${
         visible ? "block" : "hidden"
       }`}
     >
-      <div className="flex flex-col gap-2 p-3 w-max bg-[#151718] rounded-md">
+      <div className="flex flex-col md:gap-2 p-3 w-max bg-[#151718] rounded-md">
         <div
           className="w-full flex justify-end"
           onClick={() => setVisible(false)}
         >
-          <X className="text-white cursor-pointer" />
+          <X className="text-white cursor-pointer w-3 md:w-5" />
         </div>
-        <div className="flex flex-col gap-8 items-center pt-2 pb-10 px-9">
-          <h1 className="font-opsans font-bold text-sm md:text-xl text-white">
+        <div className="flex flex-col gap-3 md:gap-8 items-center pt-2 pb-3 md:pb-10 px-5 md:px-9">
+          <h1 className="font-opsans font-bold text-[11px] md:text-xl text-white">
             Connect a Wallet
           </h1>
           <div className="flex flex-col gap-5">
@@ -41,15 +41,15 @@ function LoginModal() {
                 );
               }}
             </ConnectKitButton.Custom>
-            {options.map((option, index) => (
+            {/* {options.map((option, index) => (
               <LoginModalOptions
                 key={index}
                 icon={option.icon}
                 text={option.text}
               />
-            ))}
+            ))} */}
           </div>
-          <p className="font-opsans text-white">
+          <p className="font-opsans text-white text-[11px]">
             You can't wait?{" "}
             <span className="text-button cursor-pointer">
               Explore Gameboc as Guest
@@ -61,14 +61,14 @@ function LoginModal() {
   );
 }
 
-const options: ModalOptionsProps[] = [
-  {
-    icon: nfid,
-    text: "Connect with NFID",
-  },
-  {
-    icon: identity,
-    text: "Connect with Internet Identity",
-  },
-];
+// const options: ModalOptionsProps[] = [
+//   {
+//     icon: nfid,
+//     text: "Connect with NFID",
+//   },
+//   {
+//     icon: identity,
+//     text: "Connect with Internet Identity",
+//   },
+// ];
 export default LoginModal;
--- frontend/src/components/Modals/LoginModalOptions.tsx
+++ frontend/src/components/Modals/LoginModalOptions.tsx
@@ -11,11 +11,11 @@ function LoginModalOptions({ icon, text }: ModalOptionsProps) {
   const { setVisible } = useModal;
   return (
     <div
-      className="cursor-pointer flex gap-3 justify-start items-center pl-8 pr-28 py-5 hover:bg-[#353763] bg-[#222226] rounded-md"
+      className="cursor-pointer flex gap-3 justify-start items-center pl-2 pr-7 py-2 md:pl-8 md:pr-28 md:py-5 hover:bg-[#353763] bg-[#222226] rounded-md"
       onClick={() => setVisible(false)}
     >
       <img src={icon} alt="connect icon" className="w-5" />
-      <p className="font-opsans font-bold text-[#FBFBFC] ">{text}</p>
+      <p className="font-opsans font-bold text-[#FBFBFC] text-[10px]">{text}</p>
     </div>
   );
 }
